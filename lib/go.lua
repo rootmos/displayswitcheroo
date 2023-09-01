8b9eab08e48d4b03b471f4f88cec4837b0e005fc
Gustav Behm <me@rootmos.io>
2023-09-01 05:36:00 +0200

    Adapt go.lua to mark modes xrandr-cli-style

--- lib/go.lua
+++ lib/go.lua
@@ -1,20 +1,18 @@
 local X11 = require("x11")
 local dpy <close> = X11.connect()
-print(string.format("root: %d", dpy.root))
-print(string.format("screen: %d", dpy.screen))
-print(string.format("xrandr: %s", dpy.xrandr.version))
 
 local setup = dpy.xrandr:fetch();
 
 for n, o in pairs(setup.outputs) do
     assert(n == o.name)
-    print(string.format("output %s: connected=%s", n, o.connected))
-
-    print(string.format("  crtc: %s", o.crtc))
-
-    for _, m in ipairs(o.modes) do
-        local p = o.modes:is_preferred(m) and "+" or " "
-        print(string.format(" %s%dx%d %fHz", p, m.width, m.height, m.refresh_rate))
+    print(string.format("output %s", n))
+    if o.connected then
+        local m0 = o.crtc.mode
+        for _, m in ipairs(o.modes) do
+            local p = o.modes:is_preferred(m) and "+" or " "
+            local c = m == m0 and "*" or " "
+            print(string.format(" %s%s%dx%d %fHz", c, p, m.width, m.height, m.refresh_rate))
+        end
     end
 end
 
@@ -28,23 +26,5 @@ for n, m in pairs(setup.monitors) do
 
     for _, o in ipairs(m.outputs) do
         print(string.format("  output %s", o.name))
-    end
-end
-
-for i, c in ipairs(setup.crtcs) do
-    assert(i == c.index + 1)
-    if c.enabled then
-        print(string.format("crtc %d: %dx%d+%d+%d %.2fHz", c.index,
-            c.width, c.height, c.x, c.y, c.mode.refresh_rate))
-    else
-        print(string.format("crtc %d", c.index))
-    end
-
-    for _, o in ipairs(c.outputs) do
-        print(string.format("  connected output %s", o.name))
-    end
-
-    for _, o in ipairs(c.outputs.possible) do
-        print(string.format("  possible output %s", o.name))
     end
 end
