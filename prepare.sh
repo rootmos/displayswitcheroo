7bd47764842a6ab00850cdf787dcaebdf41e2626
Gustav Behm <me@rootmos.io>
2023-11-15 08:45:45 +0100

    Build on latest ubuntu and archlinux as well

--- prepare.sh
+++ prepare.sh
@@ -18,21 +18,38 @@ done
 shift $((OPTIND-1))
 
 if [ -z "$DISTRO" ]; then
-    DISTRO=$(lsb_release -is)
+    if command -v lsb_release; then
+        DISTRO=$(lsb_release -is)
+    elif command -v pacman >/dev/null; then
+        DISTRO="Arch"
+    elif command -v apt-get >/dev/null; then
+        # TODO: debian
+        DISTRO="Ubuntu"
+    else
+        echo "unable to figure out distribution: $DISTRO" 1>&2
+        exit 1
+    fi
 fi
 echo "distro: $DISTRO" 1>&2
 
-if [ "$DISTRO" = "Arch" ]; then
+if [ "$DISTRO" = "Arch" ] || command -v pacman >/dev/null; then
     if [ -n "$UPDATE" ]; then
-        $SUDO pacman -Sy
+        $SUDO pacman -Sy 1>&2
     fi
-    $SUDO pacman -S lua libxrandr
-elif [ "$DISTRO" = "Ubuntu" ]; then
+    $SUDO pacman -S --noconfirm 1>&2 \
+        git \
+        make gcc pkgconf \
+        python gawk \
+        lua libxrandr
+elif [ "$DISTRO" = "Ubuntu" ] || command -v apt-get >/dev/null; then
     if [ -n "$UPDATE" ]; then
         $SUDO apt-get update 1>&2
     fi
-    $SUDO apt-get install --yes --no-install-recommends 1>&2 \
-        make \
+    $SUDO apt-get install --yes 1>&2 \
+        --no-install-recommends --no-install-suggests \
+        git ca-certificates \
+        make gcc pkg-config \
+        python3 gawk \
         liblua5.4-dev libxrandr-dev
     echo "LUA_PKG=lua54"
 else
